9e74a871a834a63a34f9316e52eb4501cdc4ea41
Maaz Fitter <maazfitter@gmail.com>
2023-01-31 22:33:58 -0600

    Sign In UI Complete, Sign UP WIP

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import './App.css';
 const AuthScreen = () => {
 
   return (
-    <div className="App">
-      Sign up
+    <div className="Full-page-container">
+      <div className="Left-side-container Left-side-container-img">
+        <div className='Left-side-container-content'>
+          <div>
+          <p class="Left-side-container-text">Already have a Logicly account?</p>
+          </div>
+          <button class="Left-side-container-btn-signin">Sign in</button>
+        </div>
+      </div>
+      <div className="Right-side-container">
+        <div className='Right-side-container-form-container'></div>
+      </div>
     </div>
   );
 };
